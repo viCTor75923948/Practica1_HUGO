c40d7aa9be385f5d3d436d772aa2d94910c0e3c2
viCTor75923948 <90221332+viCTor75923948@users.noreply.github.com>
2021-09-07 17:56:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-# Practica1_HUGO
+index.JS
+
+function get IMC(){
+  var peso:parse Float(document.get Element By id
+    (¨peso´).valve);
+  
+  var altura:parse Float(document.get Element By id 
+    ("altura").valve); 
+  
+  var imc:peso(altura*altura);
+  document.get Element By id("imc").inney HTML=IMC;
+}
